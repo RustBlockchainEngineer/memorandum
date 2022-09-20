c53ca7894e5b19cd96e7284f28ae745694ab5a40
SolanaEngineer <topstack2021a@gmail.com>
2022-09-20 09:04:25 +0300

    add: error.rs

---
+++ programs/memorandum/src/error.rs
@@ -0,0 +1,27 @@
+use anchor_lang::prelude::*;
+
+#[error_code]
+pub enum MemorandumError {
+    #[msg("You are not authorized to perform this action.")]
+    Unauthorized,
+    #[msg("AlreadyInUse")]
+    AlreadyInUse,
+    #[msg("InvalidProgramAddress")]
+    InvalidProgramAddress,
+    #[msg("InvalidOwner")]
+    InvalidOwner,
+    #[msg("NotAllowed")]
+    NotAllowed,
+    #[msg("Math operation overflow")]
+    MathOverflow,
+    #[msg("InvalidAccountInput")]
+    InvalidAccountInput,
+    #[msg("InvalidPubkey")]
+    InvalidPubkey,
+    #[msg("InvalidAmount")]
+    InvalidAmount,
+    #[msg("The provided program data is incorrect.")]
+    InvalidProgramData,
+    #[msg("The provided program upgrade authority is incorrect.")]
+    InvalidProgramUpgradeAuthority,
+}
